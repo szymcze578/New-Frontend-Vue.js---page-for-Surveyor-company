95cf351c16ee86879260bd8efebe443843132ea1
Szymon Czech <szymonpoczta@interia.pl>
2024-10-10 23:00:33 +0200

    Improve footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,34 +1,25 @@
 <template>
-  <footer class="bg-gray-800 text-white py-8 border-t-8 border-orange-400 lg:px-48 xl:px-96">
+  <footer
+    class="h-[140px] flex flex-col justify-between bg-gray-800 text-white py-8 border-t-8 border-orange-400 lg:px-48 xl:px-96"
+  >
     <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
       <div class="mb-4 md:mb-0">
         <h3 class="text-lg font-semibold">GEODIMETR</h3>
         <p>&copy; 2024 Usługi Geodezyjne GEODIMETR mgr inż. Mariusz Czech</p>
       </div>
-
-      <!-- Center Section: Navigation Links -->
-      <div class="flex space-x-4 mb-4 md:mb-0">
-        <a href="#" class="hover:underline">Home</a>
-        <a href="#" class="hover:underline">About</a>
-        <a href="#" class="hover:underline">Services</a>
-        <a href="#" class="hover:underline">Contact</a>
-      </div>
-
-      <!-- Right Section: Social Media Icons -->
-      <div class="flex space-x-4">
-        <a href="#" aria-label="Facebook" class="hover:text-gray-400">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#" aria-label="Twitter" class="hover:text-gray-400">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#" aria-label="LinkedIn" class="hover:text-gray-400">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-        <a href="#" aria-label="Instagram" class="hover:text-gray-400">
-          <i class="fab fa-instagram"></i>
+      <div class="text-3xl">
+        <a
+          href="https://www.facebook.com/profile.php?id=100069162198575"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span class="pi pi-facebook hover:text-blue-600 hover:cursor-pointer"></span>
         </a>
       </div>
+    </div>
+
+    <div class="flex justify-end">
+      <div>Autor Szymon Czech</div>
     </div>
   </footer>
 </template>
