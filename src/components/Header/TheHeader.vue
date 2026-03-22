2ba969dd83309213a993c45db69d8a17f3fa0801
Szymon Czech <szymonpoczta@interia.pl>
2026-03-22 14:36:56 +0100

    Header and flow improvements

--- src/components/Header/TheHeader.vue
+++ src/components/Header/TheHeader.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="sticky top-0 z-[999] pb-2 bg-gradient-to-r from-first to-second">
     <div
-      class="md:flex justify-between bg-white items-center relative transition-all duration-300 xl:px-20 font-sans subpixel-antialiasing font-bold whitespace-nowrap"
-      :style="{ height: isScrolled ? '60px' : '100px' }"
-    >
-      <div class="flex w-full gap-4 items-center">
-        <div class="cursor-pointer overflow-hidden">
+      class="md:flex justify-between bg-white items-center relative transition-all duration-500 xl:px-20 font-sans subpixel-antialiasing font-bold whitespace-nowrap"
+      :style="{ height: isScrolled ? '70px' : '120px' }"
+    > 
+      <div class="flex">
+        <div class="flex w-full gap-4 items-center">
+        <div class="cursor-pointer overflow-hidden w-[360px]">
           <RouterLink 
             to="/" 
-            class="block transition-all duration-500"
+            class="transition-all duration-500 h-full flex items-center"
           >
             <img 
-              v-if="!isScrolled" 
               src="/LOGO_LARGE_WB.png"
-              class="w-[360px] transition-all duration-500"
-              :style="{ height: isScrolled ? '0px' : '100px' }"
+              class="w-[360px] h-[120px] transition-all duration-500 object-contain"
+              :style="{ opacity: isScrolled ? '0' : '1' }"
             >
             <img 
-              v-if="isScrolled" 
               src="/LOGO_SMALL2.png"
-              class="w-[360px] transition-all duration-500 "
-              :style="{ height: isScrolled ? '60px' : '0px' }"
+              class="w-[360px] h-[70px] transition-all duration-500 object-contain absolute"
+              :style="{ opacity: isScrolled ? '1' : '0' }"
             >
           </RouterLink>
         </div>      
@@ -30,10 +29,13 @@
             <InfoIconComponent class="!mt-1" icon="pi pi-send" :data="['geodimetr@op.pl']"/>
           </div>
         </div> 
-        <div class="md:hidden flex text-4xl text-black cursor-pointer items-center">
+
+      </div>
+              <div class="md:hidden flex text-4xl text-black cursor-pointer items-center justify-end">
           <i :class="[isOpen ? 'bi bi-x' : 'bi bi-list']" @click="toggleMobileNavigationMenu"/>
         </div>
       </div>
+      
 
       <nav
         class="hidden md:flex md:items-center md:static absolute text-black font-medium cursor-pointer bg-white justify-end md:w-full h-full"
@@ -70,7 +72,7 @@ function toggleMobileNavigationMenu(){
 }
 
 function handleScroll() {
-  isScrolled.value = window.scrollY > 150
+  isScrolled.value = window.scrollY >= 150
 }
 
 onMounted(() => {
